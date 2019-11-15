580c0afc516834043fbddaddf12ae31c02a0c139
Michal <michal@Michals-Mac.local>
2019-11-15 23:22:41 +0100

    added littleTest about Redux fromthe course

---
+++ src/components/SongDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>Title: <em>{song.title}</em></p>
+      <p>Duration: <em>{song.duration}</em></p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -4,15 +4,19 @@ import { selectSong } from "../actions";
 
 class SongList extends React.Component {
   renderList() {
+    console.log({ propsiki: this.props });
     return this.props.songs.map(song => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary"
-            onClick={()=>{this.props.selectSong(song)}}
+            <button
+              className="ui button primary"
+              onClick={() => {
+                this.props.selectSong(song);
+              }}
             >
-                
-                Select</button>
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -20,13 +24,13 @@ class SongList extends React.Component {
     });
   }
   render() {
-      console.log({props:this.props})
+    console.log({ props: this.props });
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
-    console.log({state})
+  console.log({ state });
   return { songs: state.songs };
 };
 
